7465ba51e3193ab6b1ae231e2f1cac15656e950b
Ed Powderham <ed.powderham@travelperk.com>
2023-05-27 17:44:26 +0100

    fix: detect localhost before assigning https

--- packages/next-auth/src/utils/detect-origin.ts
+++ packages/next-auth/src/utils/detect-origin.ts
@@ -1,8 +1,13 @@
 /** Extract the origin from the environment */
 export function detectOrigin(forwardedHost: any, protocol: any) {
+  console.log(forwardedHost, protocol)
   // If we detect a Vercel environment, we can trust the host
   if (process.env.VERCEL ?? process.env.AUTH_TRUST_HOST)
-    return `${protocol === "http" ? "http" : "https"}://${forwardedHost}`
+    return `${
+      protocol === "http" || forwardedHost.includes("localhost")
+        ? "http"
+        : "https"
+    }://${forwardedHost}`
 
   // If `NEXTAUTH_URL` is `undefined` we fall back to "http://localhost:3000"
   return process.env.NEXTAUTH_URL
